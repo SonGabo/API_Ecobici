c29aa6b3bc83ea46661c697c32dadee659817995
SonGabo <gabow95k@gmail.com>
2019-02-12 22:12:42 -0600

    add permission to acces fine location

--- app/src/main/java/xyz/gabrielrohez/apiecobici/ui/main/view/MapsActivity.java
+++ app/src/main/java/xyz/gabrielrohez/apiecobici/ui/main/view/MapsActivity.java
@@ -1,9 +1,19 @@
 package xyz.gabrielrohez.apiecobici.ui.main.view;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,18 +21,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.List;
 
+import xyz.gabrielrohez.apiecobici.BuildConfig;
 import xyz.gabrielrohez.apiecobici.R;
 import xyz.gabrielrohez.apiecobici.data.Room.entity.StatusBikesEntity;
 import xyz.gabrielrohez.apiecobici.data.network.model.MyClusterItem;
 import xyz.gabrielrohez.apiecobici.ui.main.presenter.MapsPresenter;
 import xyz.gabrielrohez.apiecobici.ui.main.presenter.MapsPresenterIn;
 import xyz.gabrielrohez.apiecobici.utils.MyClusterRenderer;
+import xyz.gabrielrohez.apiecobici.utils.MyLocation;
 
 public class MapsActivity extends AppCompatActivity implements MapsView, OnMapReadyCallback {
 
@@ -47,12 +58,80 @@ public class MapsActivity extends AppCompatActivity implements MapsView, OnMapRe
         mapFragment.getMapAsync(this);
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.clear();
         presenter.getStations(this);
+
+        if (isEnablePermission()){
+            getMap().setMyLocationEnabled(true);
+            MyLocation.LocationResult locationResult = new MyLocation.LocationResult(){
+                @Override
+                public void gotLocation(Location location){
+                    // Position the map.
+                    getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 14));
+
+                }
+            };
+            MyLocation myLocation = new MyLocation();
+            myLocation.getLocation(this, locationResult);
+        }else
+            // Position the map.
+            getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(19.380929, -99.164088), 10));
     }
 
+    private boolean isEnablePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            return false;
+        } else
+            return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    onMapReady(mMap);
+                } else {
+                    // permission denied, boo! Disable the
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle(R.string.need_permission);
+                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            openPermissionSettings(MapsActivity.this);
+                        }
+                    });
+                    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                return;
+            }
+        }
+    }
+
+    public static void openPermissionSettings(Activity activity) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + activity.getPackageName()));
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+    /**
+     * get map
+     * @return
+     */
     protected GoogleMap getMap() {
         return mMap;
     }
@@ -63,43 +142,11 @@ public class MapsActivity extends AppCompatActivity implements MapsView, OnMapRe
      */
     @Override
     public void setStationsList(List<StatusBikesEntity> list) {
-        // Position the map.
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(19.380929, -99.164088), 10));
 
         // Initialize the manager with the context and the map.
         // (Activity extends context, so we can pass 'this' in the constructor.)
         mClusterManager = new ClusterManager<MyClusterItem>(this, getMap());
         mClusterManager.setRenderer(new MyClusterRenderer(this, getMap(), mClusterManager));
-        mClusterManager.getMarkerCollection().setOnInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                View v = getLayoutInflater().inflate(R.layout.custom_info_window, null);
-
-
-                /*TextView title = (TextView) v.findViewById(R.id.t);
-                TextView t1 = (TextView) v.findViewById(R.id.t1);
-                TextView t2 = (TextView) v.findViewById(R.id.t2);
-                ImageView imageView = (ImageView) v.findViewById(R.id.profile_image);
-                Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "font/AvenirLTStd_Medium.otf");
-                t1.setTypeface(font);
-                t2.setTypeface(font);
-
-                String name = marker.getTitle();
-                title.setText(name);
-                String info = marker.getSnippet();
-                t1.setText(info);
-                driver_id = String.valueOf((Long) marker.getId());
-                drivername = marker.getTitle();
-
-                txt_info.setText(info);*/
-                return null;
-            }
-
-            @Override
-            public View getInfoContents(Marker marker) {
-                return null;
-            }
-        });
 
         mClusterManager.setOnClusterClickListener(mClusterClickListener);
         mClusterManager.setOnClusterItemClickListener(mClusterItemClickListener);
@@ -148,4 +195,10 @@ public class MapsActivity extends AppCompatActivity implements MapsView, OnMapRe
         }
     };
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        onMapReady(mMap);
+
+    }
 }
